e952b5df688cc1274425c16edf3af74b41bbc082
CatW-M <wheelermushlin@gmail.com>
2022-11-07 20:18:16 -0700

    feat: OpportunitiesDetail conditional added

--- src/components/OpportunitiesDetail.js
+++ src/components/OpportunitiesDetail.js
@@ -29,14 +29,14 @@ const OpportunitiesDetail = (user) => {
         setProfile(response.data);
     };
 
-    
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(`------>TESTERT ${profile.email}`);
         console.log(profile);
         console.log(`%%%%%%%%%%%%%%%%%%%%%%%%%% ${data.opportunities._id}`);
-        axios.put(`${REACT_APP_SERVER_URL}/opportunities/register/${data.opportunities._id}`, {email: `${profile.email}`})
+        axios.put(`${REACT_APP_SERVER_URL}/opportunities/register/${data.opportunities._id}`, { email: `${profile.email}` })
             //need to push userid into users array in Opportunity 
             .then(response => {
                 console.log('===> Yay, update was made');
@@ -46,6 +46,39 @@ const OpportunitiesDetail = (user) => {
             })
             .catch(error => console.log('===> Error in Submission', error));
     }
+
+    const handleDelete = (e) => {
+        e.preventDefault();
+        console.log(`------>TESTERT ${profile.email}`);
+        console.log(profile);
+        console.log(`%%%%%%%%%%%%%%%%%%%%%%%%%% ${data.opportunities._id}`);
+        axios.put(`${REACT_APP_SERVER_URL}/opportunities/remove/${data.opportunities._id}`, { email: `${profile.email}` })
+            .then(response => {
+                console.log('===> User is no longer registered');
+                console.log(response);
+                setIsRegistered(false);
+
+            })
+            .catch(error => console.log('===> Error in Submission', error));
+    }
+
+    if (isregistered) {
+
+        return (<div>
+            <div>
+                <h3>You are registered for </h3>
+                <h2>{data.opportunities.name}</h2>
+                <h4>{`${new Date(data.opportunities.date).getMonth() + 1}-${new Date(data.opportunities.date).getDate()}-${new Date(data.opportunities.date).getFullYear()}`}</h4>
+                <h4>{data.opportunities.startTime} until {data.opportunities.endTime}</h4>
+                <h5>{data.opportunities.description}</h5>
+            </div>
+            {/* <div>hours component here, Daniela</div> */}
+            <div>
+            <button onClick={handleDelete} className="btn btn-primary float-right">Unregister</button>
+            </div>
+        </div>)
+    }
+
     return (
         <div className="row mt-4">
             <div className="col-md-7 offset-md-3">
@@ -59,12 +92,10 @@ const OpportunitiesDetail = (user) => {
                         <h4>{data.opportunities.startTime} until {data.opportunities.endTime}</h4>
                         <h5>{data.opportunities.description}</h5>
 
-                        <form onSubmit={handleSubmit}>
-                            <div className="form-group">
-                                <input type="hidden" name="id" value={user.id} className="form-control" />
-                            </div>
-                            <button type="submit" className="btn btn-primary float-right">Register Now!</button>
-                        </form>
+                        <div>
+                            <button type="submit" onClick={handleSubmit} className="btn btn-primary float-right">Register Now!</button>
+
+                        </div>
 
                     </div>
                 </div>
